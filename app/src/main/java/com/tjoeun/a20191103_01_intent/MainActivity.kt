319f10559a592b24747a6b8b206ed30ee68c02c3
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-04 22:04:50 +0900

    문자 전송 화면으로 이동하는 예제   - putExtra가 가능함

--- app/src/main/java/com/tjoeun/a20191103_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191103_01_intent/MainActivity.kt
@@ -46,6 +46,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)*/
         }
 
+        smsBtn.setOnClickListener {
+            var uri = Uri.parse("smsto:01093686483")
+            var intent = Intent(Intent.ACTION_SENDTO,uri)
+
+            intent.putExtra("sms_body","우리가 만든 앱입니다.")
+
+            startActivity(intent)
+        }
+
     }
 
 
