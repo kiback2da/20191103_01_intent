e27f8eff390f7c858a446758d5e6c437ff09dd8d
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-04 19:45:34 +0900

    이름 입력 화면 그리기

---
+++ app/src/main/java/com/tjoeun/a20191103_01_intent/EditUserInfoAcvity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191103_01_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditUserInfoAcvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user_info_acvity)
+    }
+}
