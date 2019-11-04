8e957c07a1720722a307cfc7f5200a58daa9209f
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-04 19:48:41 +0900

    버튼을 눌러서 사용자 정보 수정화면으로 이동

--- app/src/main/java/com/tjoeun/a20191103_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191103_01_intent/MainActivity.kt
@@ -2,11 +2,21 @@ package com.tjoeun.a20191103_01_intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameInputBtn.setOnClickListener {
+
+            var intent = intent(this,EditUserInfoAcvity,class::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
