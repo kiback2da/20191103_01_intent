4f54d5de5ab5c5e81f3ec342a585447a44c4170c
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-04 19:59:19 +0900

    확인 버튼 누르면 입력값을 intent에 담아서 결과로 전송

--- app/src/main/java/com/tjoeun/a20191103_01_intent/EditUserInfoAcvity.kt
+++ app/src/main/java/com/tjoeun/a20191103_01_intent/EditUserInfoAcvity.kt
@@ -1,5 +1,7 @@
 package com.tjoeun.a20191103_01_intent
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_user_info_acvity.*
@@ -14,6 +16,15 @@ class EditUserInfoAcvity : AppCompatActivity() {
             finish()
         }
 
+        okBtn.setOnClickListener {
+            var name = nameEdt.text
+            var intent = Intent()
+
+            intent.putExtra("inputName",nameEdt.text.toString())
+            setResult(Activity.RESULT_OK,intent)
+
+            finish()
+        }
 
 
     }
